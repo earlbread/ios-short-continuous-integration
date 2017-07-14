ba557d7d5b977113d32c3b528f9089f077dd69c4
Seunghun Lee <waydi1@gmail.com>
2017-07-14 12:44:43 +0900

    Set indentation to 2 spaces

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -10,28 +10,28 @@ import XCTest
 @testable import RomanNumeralCalculator
 
 class RomanNumeralCalculatorTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testToRoman() {
+    XCTAssertEqual(toRoman(1), "I")
+    XCTAssertEqual(toRoman(2), "II")
+    XCTAssertEqual(toRoman(4), "IV")
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    func testToRoman() {
-      XCTAssertEqual(toRoman(1), "I")
-      XCTAssertEqual(toRoman(2), "II")
-      XCTAssertEqual(toRoman(4), "IV")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
